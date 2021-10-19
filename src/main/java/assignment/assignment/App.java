705c66eb977888f8297fcde7b48e047a4c785125
lfxsecuser <nvelagapudi+lfxuser@contractor.linuxfoundation.org>
2021-10-19 22:24:17 +0530

    add

---
+++ src/main/java/assignment/assignment/App.java
@@ -0,0 +1,16 @@
+package assignment.assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String password = "lfxtest";
+        String token = "vd54fghrthy4y56y6";
+        String apikey = "tirt349875304986-0jfwdotie0";
+    }
+}
